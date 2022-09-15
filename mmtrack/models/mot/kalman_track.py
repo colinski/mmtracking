553d342ff172dbc390ddc7fa6cf8002fb575e591
Colin <csamplawski@cs.umass.edu>
2022-09-15 16:41:52 -0400

    added mocap tracl

--- mmtrack/models/mot/kalman_track.py
+++ mmtrack/models/mot/kalman_track.py
@@ -79,3 +79,62 @@ class KalmanTrack(torch.nn.Module):
         if self.time_since_update > 0:
             self.hit_streak = 0
         self.time_since_update += 1
+
+class MocapTrack(torch.nn.Module):
+    count = 0 #global count across tracks for id
+    def __init__(self, mean, cov):
+        super().__init__()
+        self.time_since_update = 0
+        self.id = MocapTrack.count
+        MocapTrack.count += 1
+        self.hit_streak = 0
+        self.age = 0
+        
+        #bbox is 1 x 6 
+        # bbox = det[0:4]
+        # self.score = det[4].cpu()
+        # self.label = det[5].cpu()
+        
+        # mean = xyxy_to_xyar(bbox).cpu()
+        # cov = torch.eye(4)
+        #cov[2:, 2:] *= 10
+        self.dymodel = NcpContinuous(3, 0.01)
+        #self.kf = EKFState(self.dymodel, mean.cpu().unsqueeze(0), cov.cpu().unsqueeze(0), time=0)
+        self.kf = EKFState(self.dymodel, mean.cpu(), torch.diag(cov).cpu(), time=0)
+    
+    @property
+    def wasupdated(self):
+        return self.time_since_update < 1
+    
+    @property
+    def state(self):
+        mean = self.kf.mean.cuda()
+        state = mean
+        # state = xyar_to_xyxy(mean)
+        return state
+
+    @property
+    def mean(self):
+        return self.kf.mean
+
+    @property
+    def cov(self):
+        return self.kf.cov
+
+    def update(self, mean, cov):
+        # bbox = det[0:4]
+        self.time_since_update = 0
+        self.hit_streak += 1
+        # bbox = xyxy_to_xyar(bbox).cpu()
+        # cov = torch.eye(4) * 0.01
+        # cov = cov
+        m = PositionMeasurement(mean.cpu(), torch.diag(cov).cpu(), time=self.kf.time)
+        self.kf, _ = self.kf.update(m)
+
+    def predict(self):        
+        self.age += 1
+        self.kf = self.kf.predict(dt=self.age)        
+        
+        if self.time_since_update > 0:
+            self.hit_streak = 0
+        self.time_since_update += 1
