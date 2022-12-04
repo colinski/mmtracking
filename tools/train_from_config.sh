573a7d3f1fe27e790edb1dc9cb36076484218223
Colin <csamplawski@cs.umass.edu>
2022-12-04 11:21:45 -0500

    update to train script

--- tools/train_from_config.sh
+++ tools/train_from_config.sh
@@ -3,4 +3,7 @@
 CONFIG=$1
 NAME=$(basename $1 .py)
 EXPDIR=logs/$NAME
+if [ -d $EXPDIR ]; then
+    rm -rf $EXPDIR/*
+fi
 singularity run --nv -H $WORK $WORK/sif/python.sif $WORK/src/mmtracking/tools/dist_train.sh $CONFIG $2 --work-dir $EXPDIR/log --seed 5
