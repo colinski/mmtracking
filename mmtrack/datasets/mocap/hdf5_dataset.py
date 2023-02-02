08dbf92584262182ea7402641881fe16e7aeb78e
Colin <csamplawski@cs.umass.edu>
2023-02-02 14:50:49 -0500

    major clean up of code

--- mmtrack/datasets/mocap/hdf5_dataset.py
+++ mmtrack/datasets/mocap/hdf5_dataset.py
@@ -33,165 +33,35 @@ font = {#'family' : 'normal',
 
 matplotlib.rc('font', **font)
 
-def convert2dict(f, keys, fname, valid_mods, valid_nodes):
-    data = {}
-    for ms in tqdm(keys, desc='loading %s' % fname):
-        data[ms] = {}
-        for k, v in f[ms].items():
-            if k == 'mocap': #mocap or node_N
-                data[ms]['mocap'] = v[()]
-            else: #is node_N
-                if k not in valid_nodes:
-                    continue
-                data[ms][k] = {}
-                for k2, v2 in f[ms][k].items():
-                    if k2 not in valid_mods:
-                        continue
-                    if k2 == 'detected_points':
-                        data[ms][k][k2] = v2[()]
-                    else:
-                        data[ms][k][k2] = v2[:]
-    return data
-
-
-def load_chunk(fname, valid_mods, valid_nodes):
-    with h5py.File(fname, 'r') as f:
-        keys = list(f.keys())
-        keys = np.array(keys).astype(int)
-        # diffs = (keys - start_time)**2
-        # start_idx = np.argmin(diffs)
-        # diffs = (keys - end_time)**2
-        # end_idx = np.argmin(diffs)
-        # keys = keys[start_idx:end_idx]
-        keys = list(keys.astype(str))
-        data = convert2dict(f, keys, fname, valid_mods, valid_nodes)
-    return data
-
-
 @DATASETS.register_module()
 class HDF5Dataset(Dataset, metaclass=ABCMeta):
     CLASSES = None
     def __init__(self,
-                 # hdf5_fnames=[],
                  cacher_cfg=None,
-                 # fps=20,
-                 # valid_mods=['mocap', 'zed_camera_left', 'zed_camera_depth'],
-                 # valid_nodes=[1,2,3,4],
-                 # start_time=1656096536271,
-                 # end_time=1656096626261,
-                 # min_x=-2162.78244, max_x=4157.92774,
-                 # min_y=-1637.84491, max_y=2930.06133,
-                 # min_z=0.000000000, max_z=903.616290,
-                 # name='train',
-                 # uid=0,
-                 # normalized_position=False,
                  pipelines={},
                  num_past_frames=0,
                  num_future_frames=0,
                  test_mode=False,
-                 # remove_first_frame=False,
-                 # max_len=None,
                  limit_axis=True,
                  draw_cov=True,
                  truck_w=30,
                  truck_h=15,
                  include_z=False,
                  **kwargs):
-        # self.valid_mods = valid_mods
-        # self.valid_nodes = ['node_%d' % n for n in valid_nodes]
-        # self.class2idx = {'truck': 1, 'node': 0}
-        # self.min_x = min_x
-        # self.max_x = max_x
-        # self.len_x = 7000
-        # self.min_y = min_y
-        # self.max_y = max_y
-        # self.len_y = 5000
-        # self.min_z = min_z
-        # self.max_z = max_z
-        # self.len_z = 1000
-        # self.normalized_position = normalized_position
         self.truck_w = truck_w
         self.truck_h = truck_h
-        # self.include_z = include_z
-        # self.name = name
-        # self.uid = uid
         self.cacher = build_dataset(cacher_cfg)
         self.fnames, self.active_keys = self.cacher.cache()
-
-        #self.max_len = np.sqrt(7**2 + 5**2)
         self.max_len = 1
-        
-        # rank, ws = get_dist_info()
-        # self.fnames = hdf5_fnames
         self.fps = self.cacher.fps
         self.limit_axis = limit_axis
         self.draw_cov = draw_cov
         self.num_future_frames = num_future_frames
         self.num_past_frames = num_past_frames
-
         self.node_pos = None
         self.node_ids = None
-
         self.colors = ['red', 'green', 'orange', 'black', 'yellow', 'blue']
         
-        # cache_dir = f'/dev/shm/cache_{self.uid}/'
-        # if not os.path.exists(cache_dir):
-            # os.mkdir(cache_dir)
-            # data = {}
-            # for fname in hdf5_fnames:
-                # chunk = load_chunk(fname, self.valid_mods, self.valid_nodes)
-                # for ms, val in chunk.items():
-                    # if ms in data.keys():
-                        # for k, v in val.items():
-                            # if k in data[ms].keys():
-                                # data[ms][k].update(v)
-                            # else:
-                                # data[ms][k] = v
-                    # else:
-                        # data[ms] = val
-
-
-            # buffers = self.fill_buffers(data)
-            # self.active_keys = sorted(buffers[-1].keys())
-            
-            # count = 0
-            # for i in range(len(buffers)):
-                # missing = False
-                # for key in self.active_keys:
-                    # if key not in buffers[i].keys():
-                        # missing = True
-                # if missing:
-                    # count += 1
-                    # continue
-                # else:
-                    # break
-            # buffers = buffers[count:]
-
-            # if remove_first_frame:
-                # buffers = buffers[1:]
-            # if max_len is not None:
-                # buffers = buffers[0:max_len]
-            
-            
-            # final_dir = f'{cache_dir}/{self.name}'
-            # if not os.path.exists(final_dir):
-                # os.mkdir(final_dir)
-            # self.fnames = []
-            # for i in trange(len(buffers)):
-                # buff = buffers[i]
-                # fname = '%s/tmp_%09d.pickle' % (final_dir, i)
-                # with open(fname, 'wb') as f:
-                    # pickle.dump(buff, f)
-                # self.fnames.append(fname)
-    
-        # self.fnames = glob.glob(f'{cache_dir}/{self.name}/*.pickle')
-        # print(self.fnames)
-        # with open(self.fnames[-1], 'rb') as f:
-            # buff = pickle.load(f)
-            # self.active_keys = sorted(buff.keys())
-        # self.fnames = sorted(self.fnames)[0:max_len]
-        # self.nodes = set([key[1] for key in self.active_keys if 'node' in key[1]])
-
         self.pipelines = {}
         for mod, cfg in pipelines.items():
             self.pipelines[mod] = Compose(cfg)
@@ -200,118 +70,9 @@ class HDF5Dataset(Dataset, metaclass=ABCMeta):
         self.flag = np.zeros(len(self), dtype=np.uint8) #ones?
     
     def __len__(self):
-        #return len(self.buffers)
         return len(self.fnames)
     
-    # def init_buffer(self):
-        # buff = {}
-        # buff['zed_camera_left'] = np.zeros((10, 10, 3)).astype(np.uint8) #will be resized
-        # buff['zed_camera_left'] = cv2.imencode('.jpg', buff['zed_camera_left'])[1] #save compressed
-        # buff['zed_camera_depth'] = np.zeros((10, 10, 1)).astype(np.int16) #will be resized
-        # buff['range_doppler'] = np.zeros((10, 10))
-        # buff = {k: v for k, v in buff.items() if k in self.valid_mods}
-        # buff['missing'] = {}
-        # for mod in self.MODALITIES:
-            # buff['missing'][mod] = True
-        # return buff
-
-    def fill_buffers(self, all_data):
-        assert 1==2
-        buffers = []
-        # buff = self.init_buffer()
-        buff = {}
-        factor = 100 // self.fps
-        num_frames = 0
-        keys = sorted(list(all_data.keys()))
-        prev_num_objs = None
-        for time in tqdm(keys, desc='filling buffers'):
-            save_frame = False
-            data = all_data[time]
-            for key in data.keys():
-                if key == 'mocap':
-                    mocap_data = json.loads(data['mocap'])
-                    if self.normalized_position:
-                        gt_pos = torch.tensor([d['normalized_position'] for d in mocap_data])
-                    else:
-                        gt_pos = torch.tensor([d['position'] for d in mocap_data])
-                        gt_pos[..., 0] += np.abs(self.min_x)
-                        gt_pos[..., 1] += np.abs(self.min_y)
-                        gt_pos[..., 2] += np.abs(self.min_z)
-                        gt_pos /= 10
-                    gt_rot = torch.tensor([d['rotation'] for d in mocap_data])
-                    # gt_rot = torch.cat([gt_rot[..., 0:2], gt_rot[..., 3:5]], dim=-1)
-                    
-                    corners, grids = [], []
-                    for k in range(len(gt_rot)):
-                        angle = rot2angle(gt_rot[k], return_rads=False)
-                        rec, grid = gen_rectange(gt_pos[k], angle, w=self.truck_w, h=self.truck_h)
-                        corners.append(rec.get_corners())
-                        if self.include_z:
-                            z_val = gt_pos[k][-1]
-                            z_vals = torch.ones(len(grid), 1) * z_val
-                            grid = torch.cat([grid, z_vals], dim=-1)
-                        grids.append(grid)
-
-                    grids = torch.stack(grids)
-
-                    corners = np.stack(corners)
-                    corners = torch.tensor(corners).float()
-
-                    gt_labels = torch.tensor([self.class2idx[d['type']] for d in mocap_data])
-                    gt_ids = torch.tensor([d['id'] for d in mocap_data])
-                    is_node = gt_labels == 0
-                    if self.node_pos is None: 
-                        self.node_pos = gt_pos[is_node]
-                        self.node_ids = gt_ids[is_node]
-                    
-                    # gt_pos = gt_pos[~is_node]
-                    # z_is_zero = gt_pos[:, -1] == 0.0
-                    # if torch.any(z_is_zero):
-                        # continue
-                    final_mask = ~is_node 
-                    if not self.include_z:
-                        gt_pos = gt_pos[..., 0:2]
-                    gt_pos = gt_pos[final_mask]
-                    gt_grid = grids[final_mask] 
-                    gt_rot = gt_rot[final_mask]
-                    gt_ids = gt_ids[final_mask] - 4
-                    if len(gt_pos) < 2:
-                        zeros = torch.zeros(2 - len(gt_pos), gt_pos.shape[-1])
-                        gt_pos = torch.cat([gt_pos, zeros - 1])
-                        
-                        zeros = torch.zeros(2 - len(gt_grid), 450, 2)
-                        gt_grid = torch.cat([gt_grid, zeros - 1])
-
-                        zeros = torch.zeros(2 - len(gt_rot), 9)
-                        gt_rot = torch.cat([gt_rot, zeros - 1])
-                        
-                        zeros = torch.zeros(2 - len(gt_ids))
-                        gt_ids = torch.cat([gt_ids, zeros - 1])
-                        
-                    buff[('mocap', 'mocap')] = {
-                        'gt_positions': gt_pos,
-                        #'gt_labels': gt_labels[final_mask].long(),
-                        'gt_ids': gt_ids.long(),
-                        'gt_rot': gt_rot,
-                        #'gt_corners': corners[final_mask],
-                        'gt_grids': gt_grid
-                    }
-                    num_frames += 1
-                    save_frame = True
-
-                if 'node' in key:
-                    for k, v in data[key].items():
-                        if k in self.valid_mods:
-                            buff[(k, key)] = v
-            
-            if save_frame and num_frames % factor == 0:
-                new_buff = copy.deepcopy(buff)
-                buffers.append(new_buff)
-        return buffers
-
-    
     def apply_pipelines(self, buff):
-        #new_buff = {'missing': buff['missing']}
         new_buff = {}
         for key, val in buff.items():
             mod, node = key
@@ -327,10 +88,8 @@ class HDF5Dataset(Dataset, metaclass=ABCMeta):
         return buff
     
     def __getitem__(self, ind):
-        #buff = self.buffers[ind]
         buff = self.read_buff(ind)
         new_buff = self.apply_pipelines(buff)
-        # new_buff['ind'] = ind
         
         idx_set = torch.arange(len(self))
         start_idx = max(0, ind - self.num_past_frames)
@@ -348,28 +107,16 @@ class HDF5Dataset(Dataset, metaclass=ABCMeta):
             future_idx = torch.cat([future_idx, zeros + len(self) - 1])
         
         buffs = []
-        # new_buff['past_frames'] = []
         for idx in past_idx:
-           # buff = self.buffers[idx]
             buff = self.read_buff(idx)
             buff = self.apply_pipelines(buff)
-            # buff['ind'] = idx
             buffs.append(buff)
-            # new_buff['past_frames'].append(buff)
-        
         buffs.append(new_buff)
 
-        # new_buff['future_frames'] = []
         for idx in future_idx:
-            # buff = self.buffers[idx]
             buff = self.read_buff(idx)
             buff = self.apply_pipelines(buff)
-            # buff['ind'] = idx
             buffs.append(buff)
-            # new_buff['future_frames'].append(buff)
-        
-        #merge time series into a batch
-        # res = mmcv.parallel.collate(buffs)
         return buffs
     
     def eval_mot(self, outputs, **eval_kwargs):
@@ -484,10 +231,8 @@ class HDF5Dataset(Dataset, metaclass=ABCMeta):
     def write_video(self, outputs, **eval_kwargs): 
         logdir = eval_kwargs['logdir']
         fname = f'{logdir}/latest_vid.mp4'
-        #fig, axes = init_fig(self.active_keys, len(self.nodes))
         fig, axes = init_fig(self.active_keys)
         size = (fig.get_figwidth()*50, fig.get_figheight()*50)
-        # size = (fig.get_figheight()*100, fig.get_figwidth()*100)
         size = tuple([int(s) for s in size])
         vid = cv2.VideoWriter(fname, cv2.VideoWriter_fourcc(*'mp4v'), self.fps, size)
 
@@ -513,11 +258,6 @@ class HDF5Dataset(Dataset, metaclass=ABCMeta):
                         axes[key].set_ylim(0,500)
                         axes[key].set_aspect('equal')
                     
-                    # for j in range(len(self.node_pos)):
-                        # pos = self.node_pos[j]
-                        # ID = self.node_ids[j] + 1
-                        # axes[key].scatter(pos[0], pos[1], marker=f'${ID}$', color='black', lw=1, s=20*4**2)
-                    
                     num_gt = len(val['gt_positions'])
                     for j in range(num_gt):
                         pos = val['gt_positions'][j]
@@ -536,11 +276,7 @@ class HDF5Dataset(Dataset, metaclass=ABCMeta):
                         axes[key].add_patch(rec)
 
                         r=self.truck_w/2
-                        axes[key].arrow(pos[0], pos[1], r*rot[0], r*rot[1], head_width=0.05, head_length=0.05, fc=color, ec=color)
-
-                        # axes[key].scatter(grid[...,0], grid[...,1], s=0.5, color='red')
-
-                        # axes[key].text(pos[0], pos[1], '%0.2f %0.2f\n%0.2f %0.2f' % (rot[0],rot[1], rot[3], rot[4]))
+                        axes[key].arrow(pos[0], pos[1], r*rot[0], r*rot[1], head_width=0.05*100, head_length=0.05*100, fc=color, ec=color)
                             
                     
                     if len(outputs['det_means']) > 0:
