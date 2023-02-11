0fb06577650f3fee43297f761d768c174b75c87c
Colin <csamplawski@cs.umass.edu>
2023-02-11 20:29:50 +0000

    update to test scripts

--- tools/test_from_config.sh
+++ tools/test_from_config.sh
@@ -3,5 +3,7 @@ CONFIG=$1
 NAME=$(basename $1 .py)
 EXPDIR=logs/$NAME
 
+mkdir -p $EXPDIR/test
+
 singularity run --nv -H $WORK $WORK/sif/python.sif python $WORK/src/mmtracking/tools/cache_datasets.py $CONFIG
-singularity run --nv -H $WORK $WORK/sif/python.sif $WORK/src/mmtracking/tools/dist_test.sh $CONFIG $2 --checkpoint $EXPDIR/latest.pth --eval vid track --cfg-options evaluation.logdir=$EXPDIR evaluation.video_length=500 evaluation.dataset=test
+singularity run --nv -H $WORK $WORK/sif/python.sif $WORK/src/mmtracking/tools/dist_test.sh $CONFIG $2 --checkpoint $EXPDIR/latest.pth --eval vid track --cfg-options evaluation.logdir=$EXPDIR/test evaluation.video_length=500 evaluation.dataset=test
