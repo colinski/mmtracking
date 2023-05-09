948ab5c6fcca285e94d9c73f617749affa974846
Colin <csamplawski@cs.umass.edu>
2023-05-09 17:32:19 -0400

    confirm to new args stucture

--- tools/train_val_test_from_dir.sh
+++ tools/train_val_test_from_dir.sh
@@ -9,4 +9,4 @@ fi
 singularity run --nv -H $WORK $WORK/sif/python.sif $WORK/src/mmtracking/tools/dist_train.sh $DIR/config.py $2 --work-dir $EXPDIR --seed 5
 rm $DIR/log/config.py
 singularity run --nv $WORK/sif/python.sif python $WORK/src/mmtracking/tools/tune.py $DIR
-singularity run --nv $WORK/sif/python.sif python $WORK/src/mmtracking/tools/test_from_tune.py $DIR
+singularity run --nv $WORK/sif/python.sif python $WORK/src/mmtracking/tools/test_from_tune.py $DIR --write_video --save_outputs
